b4495ef38f051957a84769b54514b9917aca94b1
Victor Oka <oka.victor97@gmail.com>
2019-07-11 18:02:19 -0300

    demo finished

--- SwiftUI Demo/ContentView.swift
+++ SwiftUI Demo/ContentView.swift
@@ -11,20 +11,35 @@ import SwiftUI
 // MARK: - Main ContentView
 struct ContentView : View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
+        VStack {
+            // Map
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
             
-            HStack {
-                Text("Josua Tree National Park")
-                    .font(.subheadline)
-                    .color(.secondary)
-                // The spacer object takes all the space that it can by default
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-        }.padding()
+            // Image
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            // TextViews
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Josua Tree National Park")
+                        .font(.subheadline)
+                        .color(.secondary)
+                    // The spacer object takes all the space that it can by default
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }.padding()
+            // Spacer
+            Spacer()
+        }
     }
 }
 
--- SwiftUI Demo/MapView.swift
+++ SwiftUI Demo/MapView.swift
@@ -7,10 +7,23 @@
 //
 
 import SwiftUI
+import MapKit
 
-struct MapView : View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+struct MapView : UIViewRepresentable {
+    
+    // This method required changes to MKMapView type
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        // Put the code to update the UIKit view
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        // Sets the map position for the preview
+        uiView.setRegion(region, animated: true)
     }
 }
 
